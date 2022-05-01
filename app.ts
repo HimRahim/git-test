6c1d3dfd7680f8a301adc48e4be8ed8be2a6f0f5
HimRahim <6210110690@psu.ac.th>
2022-05-01 17:46:11 +0700

    typecritp-1.2

--- app.ts
+++ app.ts
@@ -1,3 +1,6 @@
 let text: string;
 text = 'Hello Wordl';
-console.log(text)
+let text2: string;
+text2 = 'Hello 2'
+console.log(text);
+console.log(text2)
---
+++ out/app.js
@@ -0,0 +1,7 @@
+var text;
+text = 'Hello Wordl';
+var text2;
+text2 = 'Hello 2';
+console.log(text);
+console.log(text2);
+//# sourceMappingURL=app.js.map
