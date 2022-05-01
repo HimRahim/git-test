a1fa812325fbb323644deb34d953428c5af12fb6
HimRahim <6210110690@psu.ac.th>
2022-05-01 18:00:27 +0700

    template string 1

--- app.ts
+++ app.ts
@@ -4,3 +4,23 @@ let text2: string;
 text2 = 'Hello 2'
 console.log(text);
 console.log(text2)
+
+
+// 1.
+let str: string = 'this is String';
+console.log(str)
+
+// 2.
+console.log(str.charAt(0))
+
+// 3.
+console.log(str[0])
+
+// 4.
+console.log(str.charAt(20))
+console.log(str[20])
+
+// 5.
+let str1: string = 'hello'
+let str2: string = 'world'
+console.log(`${str1} ${str2}`)
--- out/app.js
+++ out/app.js
@@ -4,4 +4,18 @@ var text2;
 text2 = 'Hello 2';
 console.log(text);
 console.log(text2);
+// 1.
+var str = 'this is String';
+console.log(str);
+// 2.
+console.log(str.charAt(0));
+// 3.
+console.log(str[0]);
+// 4.
+console.log(str.charAt(20));
+console.log(str[20]);
+// 5.
+var str1 = 'hello';
+var str2 = 'world';
+console.log("".concat(str1, " ").concat(str2));
 //# sourceMappingURL=app.js.map
